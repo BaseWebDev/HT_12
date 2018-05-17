3a1cfce6def09e743202191b2463e563bce5a7fb
SermusAnton <antonsermus@mail.ru>
2018-05-17 12:07:35 +0300

    Add class DateTimeExtension

---
+++ ExtensionMetod/ExtensionMetod/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ExtensionMetod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (var day in DateTime.Now.TakeDays(3)) {
+                Console.WriteLine(day);
+            }
+        }       
+    }
+    /// <summary>
+    /// Возвращает итератор со значеняими days - кол-ва дней после текущего дня
+    /// </summary>
+    static class DateTimeExtensions {
+        public static IEnumerable<DateTime> TakeDays(this DateTime dateTime, int days) {
+            for (int i = 0; i < days; i++) {
+                yield return  dateTime.AddDays(i+1); // Поскольку n слудующих за текущим днем, то +1
+            }
+        }
+
+    }
+}
